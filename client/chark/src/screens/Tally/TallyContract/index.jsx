034f7e9f6ab46ae0e17fbb16734a3ce93e207ab7
Ireshm13 <miresh@insightworkshop.io>
2023-09-16 21:38:50 +0545

    fixes on pdf not displaying

--- client/chark/src/screens/Tally/TallyContract/index.jsx
+++ client/chark/src/screens/Tally/TallyContract/index.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
-import { ActivityIndicator, StyleSheet, View, Alert } from 'react-native';
-import { WebView } from 'react-native-webview';
-import Share from 'react-native-share';
-import RNFS from 'react-native-fs';
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import { ActivityIndicator, StyleSheet, View, Alert, Text } from "react-native";
+import { WebView } from "react-native-webview";
+import Share from "react-native-share";
+import RNFS from "react-native-fs";
 
-import useSocket from '../../../hooks/useSocket';
-import { getContract } from '../../../services/tally';
+import useSocket from "../../../hooks/useSocket";
+import { getContract } from "../../../services/tally";
 
-import Button from '../../../components/Button';
-import FloatingActionButton from '../../../components/FloadingActionButton';
+import Button from "../../../components/Button";
+import FloatingActionButton from "../../../components/FloadingActionButton";
 
 const TallyContract = (props) => {
   const { tally_seq } = props.route?.params ?? {};
@@ -30,23 +30,44 @@ const TallyContract = (props) => {
   }, [props.navigation, downloading]); */
 
   useEffect(() => {
+    setContract(null);
+
     const showPDF = async () => {
       setDownloading(true);
-
-      getContract(wm, {
-        tally_seq
-      }).then((data) => {
-        setContract(data);
-      }).catch(console.log).finally(() => {
-        setDownloading(false);
-      })
-    }
+    };
 
     showPDF();
-  }, [wm, tally_seq, setContract]);
+  }, [wm, tally_seq]);
+
+  const getNewContract = () => {
+    getContract(wm, {
+      tally_seq,
+    })
+      .then((data) => {
+
+        setContract(data);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setDownloading(false);
+      });
+  };
 
   useEffect(() => {
-  }, [contract])
+    const unsubscribe = props.navigation.addListener("focus", () => {
+      setTimeout(() => getNewContract(), 100);
+    });
+
+    return unsubscribe;
+  }, [wm, tally_seq, contract]);
+
+  useEffect(() => {
+    const handler = props.navigation.addListener("blur", () => {
+      setContract(null);
+    });
+
+    return handler;
+  }, [wm, tally_seq, contract]);
 
   const onShare = () => {
     if (contract) {
@@ -55,37 +76,41 @@ const TallyContract = (props) => {
       const downloadOptions = {
         fromUrl: contract,
         toFile: downloadDest,
-      }
+      };
 
-      RNFS.downloadFile(downloadOptions).promise
-        .then(result => {
+      RNFS.downloadFile(downloadOptions)
+        .promise.then((result) => {
           if (result.statusCode === 200) {
             return downloadDest;
           }
           return "Failed to download";
-        }).then(downloadPath => {
+        })
+        .then((downloadPath) => {
           setDownloading(false);
           const shareOptions = {
-            title: 'Share Contract',
-            message: 'Please find the attached contract file.',
+            title: "Share Contract",
+            message: "Please find the attached contract file.",
             url: `file://${downloadPath}`,
-            type: 'application/pdf',
+            type: "application/pdf",
           };
           return Share.open(shareOptions);
-        }).then(result => {
-        }).catch(ex => {
+        })
+        .then((result) => {})
+        .catch((ex) => {
           setDownloading(false);
         });
     } else {
       Alert.alert("Error", "Contract not found.");
     }
-  }
+  };
 
   const renderLoading = () => {
-    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ActivityIndicator size={'large'} />
-    </View>
-  }
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  };
 
   const injectedJs = `
     const meta = document.createElement('meta'); 
@@ -94,23 +119,40 @@ const TallyContract = (props) => {
     document.getElementsByTagName('head')[0].appendChild(meta); 
   `;
 
-  return <View style={styles.container}>
-    <WebView
-      injectedJavaScript={injectedJs} 
-      scalesPageToFit={false}
-      startInLoadingState={true}
-      renderLoading={renderLoading}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      style={styles.webView}
-      source={{ 
-        uri: `https://docs.google.com/gview?embedded=true&url=${contract}`
-      }}
-    />
-    
-<FloatingActionButton onPress={onShare} type='share'  disabled={downloading}/>
-  </View>
-}
+
+
+  return (
+    <View style={styles.container}>
+      {contract ? (
+        <WebView
+          injectedJavaScript={injectedJs}
+          scalesPageToFit={false}
+          scrollEnabled={true}
+          startInLoadingState={true}
+          renderLoading={renderLoading}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          style={styles.webView}
+          source={{
+            uri: `https://docs.google.com/gview?embedded=true&url=${contract}`,
+          }}
+        />
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Loading...</Text>
+        </View>
+      )}
+
+      <FloatingActionButton
+        onPress={onShare}
+        type="share"
+        disabled={downloading}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -121,7 +163,6 @@ const styles = StyleSheet.create({
     width:"100%",
     height:"100%"
   },
-})
+});
 
 export default TallyContract;
-
